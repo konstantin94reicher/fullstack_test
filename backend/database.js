dfdd44943ee1f2b5ce2def93f7d5b39ae872a185
konstantin94reicher <205547482+konstantin94reicher@users.noreply.github.com>
2026-03-17 12:22:51 +0100

    database.js für railway angepasst

--- backend/database.js
+++ backend/database.js
@@ -1,12 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DATABASE_HOST,
+        database: process.env.DATABASE_NAME,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        port: process.env.DATABASE_PORT,
+      }
+);
 
 module.exports = pool;
